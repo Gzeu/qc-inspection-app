0bace8a8dff9a2a8e40a3e15b3af893f5b66492b
George Pricop <96602369+Gzeu@users.noreply.github.com>
2025-10-02 01:11:27 +0300

    Add missing styles import and fix build dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/globals.css'
+import './styles/components.css'
 
 // Register service worker
 if ('serviceWorker' in navigator) {
